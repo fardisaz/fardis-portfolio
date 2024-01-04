366b0af6a68827b465f86b486f96d48cc8c325cb
fardis1 <fardis@automedia.de>
2024-01-04 09:41:55 +0100

    Bring the last touches

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,9 +3,6 @@ const burger = document.querySelector("#burger-menu");
 const ul = document.querySelector("nav ul");
 const nav = document.querySelector("nav");
 
-// Scroll to top selection
-const scrollUp = document.querySelector("#scroll-up");
-
 // Select nav links
 const navLink = document.querySelectorAll(".nav-link");
 
@@ -20,12 +17,3 @@ navLink.forEach((link) =>
     ul.classList.remove("show");
   })
 );
-
-// scroll to top functionality
-scrollUp.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
-  });
-});
